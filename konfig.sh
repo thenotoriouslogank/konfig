e948f0ff10bbab0efa6d384176bd1b6cee6fedaa
logank <logan.aker1989@gmail.com>
2022-10-31 05:41:19 -0400

    Got permissions working for SSH configuration.

--- konfig.sh
+++ konfig.sh
@@ -5,119 +5,127 @@
 source .konfigrc
 
 verify() {
-    # Inform the user that the actions they're undertaking may have
-    # consequences.
-    clear
-    echo "Welcome to Konfigure."
-    echo ""
-    echo "This application will make changes to currently installed "
-    echo "software, as well as creating, moving, and editing of files "
-    echo "and/or other applications on the system."
-    echo ""
- 
-    while true; do
+        # Inform the user that the actions they're undertaking may have
+        # consequences.
+        clear
+        echo "Welcome to Konfigure."
+        echo ""
+        echo "This application will make changes to currently installed "
+        echo "software, as well as creating, moving, and editing of files "
+        echo "and/or other applications on the system."
+        echo ""
 
-    read -p "Would you like to continue? [y/n] " yn
+        while true; do
 
-    case $yn in
-            [yY] ) echo "Updating currently installed software." && prepare;
-                    break;;
-            [nN] ) echo "Exiting";
-                    exit;;
-            * ) echo "Invalid response.";;
-    esac
-    done 
+                read -p "Would you like to continue? [y/n] " yn
+
+                case $yn in
+                [yY])
+                        echo "Updating currently installed software." && prepare
+                        break
+                        ;;
+                [nN])
+                        echo "Exiting"
+                        exit
+                        ;;
+                *) echo "Invalid response." ;;
+                esac
+        done
 }
 
 prepare() {
-    # Ensure the system is up-to-date with currently installed software
-    echo ""
-    echo "If this is a newly-installed system, this may take a few minutes."
-    echo ""
-    mkdir backups
-    cd backups
-    apt update && apt upgrade -y && doInstalls
+        # Ensure the system is up-to-date with currently installed software
+        echo ""
+        echo "If this is a newly-installed system, this may take a few minutes."
+        echo ""
+        uname=$USER
+        mkdir backups
+        cd backups
+        apt update && apt upgrade -y && doInstalls
 }
 
 doInstalls() {
-    echo ""
-    apt install "${applications[@]}"
-    echo ""
-    echo "Installation(s) complete.  Some files have been"
-    echo "automatically removed via apt autoremove."
-    echo ""
+        echo ""
+        apt install "${applications[@]}"
+        echo ""
+        echo "Installation(s) complete.  Some files have been"
+        echo "automatically removed via apt autoremove."
+        echo ""
 
-    while true; do
+        while true; do
 
-    read -p "Would you like to install Microsoft Visual Studio Code? [y/n] " yn
+                read -p "Would you like to install Microsoft Visual Studio Code? [y/n] " yn
 
-    case $yn in
-            [yY] ) echo "Installing VSCode..."  \
-                    && vscode;
-                    break;;
-            [nN] ) echo "Skipping VSCode install.";
-                    break;;
-            * ) echo "Invalid response."
-    esac
-    done
+                case $yn in
+                [yY])
+                        echo "Installing VSCode..." &&
+                                vscode
+                        break
+                        ;;
+                [nN])
+                        echo "Skipping VSCode install."
+                        break
+                        ;;
+                *) echo "Invalid response." ;;
+                esac
+        done
 
-    echo ""
+        echo ""
 
-    while true; do
+        while true; do
 
-    read -p "Would you like to install GitHub Command Line Interface? [y/n] " yn
+                read -p "Would you like to install GitHub Command Line Interface? [y/n] " yn
 
-    case $yn in
-            [yY] ) echo "Installing gh..." && gh
-                    break;;
-            [nN] ) echo "Skipping gh install.";
-                    break;;
-            * ) echo "Invalid response."
-    esac
-    done
+                case $yn in
+                [yY])
+                        echo "Installing gh..." && gh
+                        break
+                        ;;
+                [nN])
+                        echo "Skipping gh install."
+                        break
+                        ;;
+                *) echo "Invalid response." ;;
+                esac
+        done
 
-    echo ""
-    echo "Cleaning up. . ."
-    echo ""
+        echo ""
+        echo "Cleaning up. . ."
+        echo ""
 
+        while true; do
 
-    while true; do
+                read -p "Would you like to continue? [y/n] " yn
 
-    read -p "Would you like to continue? [y/n] " yn
-
-    case $yn in
-            [yY] ) echo "Konfigure will now setup certain system parameters" && sysConfig;
-                    break;;
-            [nN] ) echo "Exiting";
-                    exit;;
-            * ) echo "Invalid response.";;
-    esac
-    done
+                case $yn in
+                [yY])
+                        echo "Konfigure will now setup certain system parameters" && sysConfig
+                        break
+                        ;;
+                [nN])
+                        echo "Exiting"
+                        exit
+                        ;;
+                *) echo "Invalid response." ;;
+                esac
+        done
 }
 
 sysConfig() {
-    echo ""
-    echo "Setting up SSH config files..."
-    echo ""
-    sshdConfig
-    echo "Setting up .zshrc..."
-    echo ""
-    echo "Setting up screen..."
-    echo ""
-    makeDatasheet
+        source ../.konfigrc
+        sshSetup
 }
 
 makeDatasheet() {
-    return 1
+        return 1
 }
 
 main() {
-    verify
-    #sysConfig
+        verify
 }
 
 main
 
 # TODO: Generate system info datasheet
 # TODO: Configure SSH, screen, plex, whatever else
-# TODO: Create basic I/O menus? 
+# TODO: Create basic I/O menus?
